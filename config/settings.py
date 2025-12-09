dd0f38ea3176f722da998502dff3c1e513904f0b
Chernokozov Yuriy <Chernokozovyu@gmail.com>
2025-12-10 00:03:42 +0300

    Добавлен .env.sample и отредактирован config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from dotenv import load_dotenv
 
+load_dotenv(override=True)
+
 from pathlib import Path
 
 from django.conf.global_settings import STATICFILES_DIRS
@@ -24,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--bz3z$isl-isf=mwdq&(5uvas)w%7%=g!y$y%zu)6!hzr*bfqk"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = []
 
