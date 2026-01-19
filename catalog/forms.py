11dbee87f59076e1ee98615fe5b550ba8ff08d32
Chernokozov Yuriy <Chernokozovyu@gmail.com>
2026-01-20 00:19:30 +0300

    Доработка домашней работы, исправление недочетов

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,9 +1,6 @@
-
 from django import forms
 from django.core.exceptions import ValidationError
-from django.core.files.images import get_image_dimensions
-
-from .models import Product, Category
+from .models import Product
 
 
 class ProductForm(forms.ModelForm):
@@ -15,7 +12,7 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ['name', 'description', 'image', 'category', 'price']
+        fields = ['name', 'description', 'image', 'category', 'price', 'is_published']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
             'category': forms.Select(attrs={'class': 'form-select'}),
@@ -26,15 +23,14 @@ class ProductForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         for field_name, field in self.fields.items():
-            if field_name != 'image':
-                if hasattr(field.widget, 'input_type'):
-                    if field.widget.input_type == 'checkbox':
-                        field.widget.attrs['class'] = 'from-check-input'
-                    else:
-                        field.widget.attrs['class'] = 'form-control'
-                else:
-                    field.widget.attrs['class'] = 'form-control'
+            if field_name == 'is_published':
+                # Чекбокс
+                field.widget.attrs['class'] = 'form-check-input'
+            elif field_name != 'image':
+                # Текстовые поля, select и т.д.
+                field.widget.attrs['class'] = 'form-control'
 
+            # Дополнительные атрибуты
             if field_name == 'name':
                 field.widget.attrs['placeholder'] = 'Введите название продукта'
             elif field_name == 'price':
@@ -42,21 +38,31 @@ class ProductForm(forms.ModelForm):
                 field.widget.attrs['step'] = '0.01'
                 field.widget.attrs['min'] = '0'
 
-            if 'image' in self.fields:
-                self.fields['image'].widget.attrs['class'] = 'form-control'
-                self.fields['image'].widget.attrs['accept'] = 'image/jpeg,image/png'
+        # Поле image (если есть)
+        if 'image' in self.fields:
+            self.fields['image'].widget.attrs['class'] = 'form-control'
+            self.fields['image'].widget.attrs['accept'] = 'image/jpeg,image/png'
+
+    def _check_forbidden_words(self, text, field_name):
+        """Вспомогательный метод для проверки запрещенных слов"""
+        for word in self.FORBIDDEN_WORDS:
+            if word in text:
+                raise ValidationError(f'{field_name.capitalize()} содержит запрещенное слово: "{word}"')
+        return text
 
     def clean_name(self):
         """Валидация названия на запрещенные слова"""
-        name = self.cleaned_data.get('name', '').lower()
-        self._check_forbidden_words(name, 'названии')
-        return self.cleaned_data['name']
+        name = self.cleaned_data.get('name', '')
+        if name:  # Проверяем только если есть текст
+            return self._check_forbidden_words(name.lower(), 'названии')
+        return name
 
     def clean_description(self):
         """Валидация описания на запрещенные слова"""
-        description = self.cleaned_data.get('description', '').lower()
-        self._check_forbidden_words(description, 'описании')
-        return self.cleaned_data['description']
+        description = self.cleaned_data.get('description', '')
+        if description:  # Проверяем только если есть текст
+            return self._check_forbidden_words(description.lower(), 'описании')
+        return description
 
     def clean_price(self):
         """Валидация цены"""
@@ -66,7 +72,7 @@ class ProductForm(forms.ModelForm):
         return price
 
     def clean_image(self):
-        """Валидация изображения"""
+        """Валидация изображения (дополнительное задание)"""
         image = self.cleaned_data.get('image')
 
         if not image:
@@ -78,10 +84,5 @@ class ProductForm(forms.ModelForm):
 
         if image.size > 5 * 1024 * 1024:
             raise ValidationError('Размер изображения не должен превышать 5 МБ')
-        return image
 
-    def _check_forbidden_words(self, text, field_name):
-        """Вспомогательный метод для проверки запрещенных слов"""
-        for word in self.FORBIDDEN_WORDS:
-            if word in text:
-                raise ValidationError(f'Поле содержит запрещенное слово: "{word}"')
+        return image
--- catalog/models.py
+++ catalog/models.py
@@ -31,7 +31,7 @@ class Product(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True, verbose_name="Дата последнего обновления"
     )
-    is_published = models.BooleanField(default=True, verbose_name="Опубликовано")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
 
     class Meta:
         verbose_name = "Продукт"
