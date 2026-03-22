0eea1481bb3b5c1b8c7ea7509bddd3f916cbde09
Chernokozov Yuriy <Chernokozovyu@gmail.com>
2026-03-22 20:20:08 +0300

    Fix: исправлена регистрация и верификация пользователя
    - Убран LoginRequiredMixin из UserCreateView
    - Добавлен автоматический вход после подтверждения email
    - Исправлена настройка DEFAULT_AUTO_FIELD
    - Применены миграции

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,8 @@ load_dotenv(override=True)
 
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS
+from django.conf.global_settings import STATICFILES_DIRS, AUTH_USER_MODEL, EMAIL_BACKEND, DEFAULT_FROM_EMAIL, LOGIN_URL, \
+    LOGIN_REDIRECT_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 
     "catalog",
     "blog",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,23 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Максимальный размер загружаемого файла (5MB)
 DATA_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024  # 5 MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 5 * 1024 * 1024  # 5 MB
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
+
+LOGIN_URL = '/users/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "yurytchernokozov@yandex.ru"
+EMAIL_HOST_PASSWORD = "qgeotvmoappnblni"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
